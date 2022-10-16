a5bc2aa43b95954c9cbcb31bd273f6166feb57ab
rebecca-stitt <rebeccaemma@gmail.com>
2022-10-16 21:10:21 +1300

    add package.json, index, rebeccaexplanation.js, gitignore file. update to ignore rebeccaexplanation.js file

---
+++ rebeccaexplanation.js
@@ -0,0 +1,18 @@
+//"scripts": {
+// what you want to happen on start. ie on start you want to operate the
+//index file. the
+//"start": "node index", which is our file with the cowsay info
+//from the npm site
+//can also change in the scripts:
+//"dev": "nodem index", (remember the quote marks and the comma)
+//WE WOULD USE NPM RUN DEV HERE
+
+//ASK To use nodemon, replace the word node on the command line when executing your script.
+//nodemon is a tool that helps develop node.js based applications by automatically restarting the node application
+// when file changes in the directory are detected.
+//
+//then in terminal npm run dev
+//"test": "echo \"Error: no test specified\" && exit 1"
+//const cowsay = require('cowsay') at the top from monday homework.
+//console.log shows the cowsay
+//
